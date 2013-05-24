908f25ab116eb27c6dc539eac3e239ee8204a28f
Daniela Barths <daniela.barths@googlemail.com>
2013-05-24 22:49:04 +0200

    Exceptions implementiert

--- src/cash_access/OverdraftException.java
+++ src/cash_access/OverdraftException.java
@@ -1,5 +1,15 @@
 package cash_access;
 
-public class OverdraftException {
+public class OverdraftException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+	
+	public OverdraftException() {
+		super();
+	}
+	
+	public OverdraftException(String message) {
+		super(message);
+	}
 
 }
