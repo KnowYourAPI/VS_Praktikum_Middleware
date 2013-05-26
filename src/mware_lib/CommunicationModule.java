6f619f1c031737c9e43d91d03d7d7283c7fccff8
KnowYourAPI <addictedtoicetea@gmail.com>
2013-05-26 19:48:53 +0200

    Send-Methode implementiert

--- src/mware_lib/CommunicationModule.java
+++ src/mware_lib/CommunicationModule.java
@@ -4,14 +4,20 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class CommunicationModule {
 	
 	public void send(String message, String host, int port) {
-		//TODO
+		try {
+			Socket socket = new Socket(host, port);
+			writeToServer(socket, message);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public String sendAndReceive(String message, String host, int port) {
