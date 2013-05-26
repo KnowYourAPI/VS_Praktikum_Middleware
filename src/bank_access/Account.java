519886bce1dc462423f40a3a266800e94ec0cf6f
Daniela Barths <daniela.barths@googlemail.com>
2013-05-26 13:37:06 +0200

    aufrufe stub-methoden an communication-module gesand

--- src/bank_access/Account.java
+++ src/bank_access/Account.java
@@ -1,5 +1,8 @@
 package bank_access;
 
+import mware_lib.CommunicationModule;
+import mware_lib.ObjectBroker;
+
 public class Account extends AccountImplBase{
 
 	String host;
@@ -14,11 +17,23 @@ public class Account extends AccountImplBase{
 	
 	@Override
 	public void transfer(double amount) throws OverdraftException {
+		ObjectBroker objectBroker = ObjectBroker.getInstance();
+		CommunicationModule communicationModule = objectBroker.getCommunicationModule();
+		String message = "INVOKE%" + this.name + "%transfer%double%" + amount;
+		communicationModule.send(message, this.host, this.port);
 		// TODO 
 	}
 
 	@Override
 	public double getBalance() {
+		ObjectBroker objectBroker = ObjectBroker.getInstance();
+		CommunicationModule communicationModule = objectBroker.getCommunicationModule();
+		String message = "INVOKE%" + this.name + "%getBalance";
+		communicationModule.send(message, this.host, this.port);
+		
+		// und wie komme ich jetzt an den Returnwert?
+		// ? return communicationModule.recieve();
+		
 		// TODO 
 	}
 
