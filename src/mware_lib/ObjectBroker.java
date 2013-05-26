01cfbb4a2d6160862a237e5ca085cc9cc189f854
KnowYourAPI <addictedtoicetea@gmail.com>
2013-05-26 20:48:19 +0200

    Singleton-Funktionalitaet des ObjectBrokers implementiert

--- src/mware_lib/ObjectBroker.java
+++ src/mware_lib/ObjectBroker.java
@@ -3,9 +3,33 @@ package mware_lib;
 import mware_lib.communicationModule.CommunicationModule;
 
 public class ObjectBroker {
+	
+	private static ObjectBroker instance;
 
 	public static ObjectBroker init(String serviceHost, int listenPort) {
-		//TODO
+		if(instance == null) {
+			CommunicationModule communicationModule = new CommunicationModule();
+			RemoteReferenceModule remoteReferenceModule = new RemoteReferenceModule();
+			//TODO: NameService-Implementierung
+			NameService nameService = new NameService();
+			instance = new ObjectBroker(communicationModule, remoteReferenceModule, nameService);
+		}
+		return instance;
+	}
+	
+	public static ObjectBroker getInstance() {
+		return instance;
+	}
+	
+	private CommunicationModule communicationModule;
+	private RemoteReferenceModule remoteReferenceModule;
+	private NameService nameService;
+	
+	private ObjectBroker(CommunicationModule communicationModule,
+			RemoteReferenceModule remoteReferenceModule, NameService nameService) {
+		this.communicationModule = communicationModule;
+		this.remoteReferenceModule = remoteReferenceModule;
+		this.nameService = nameService;
 	}
 	
 	public NameService getNameService() {
@@ -17,10 +41,6 @@ public class ObjectBroker {
 	}
 	
 	//------ weitere benoetigte Methoden ------
-	
-	public static ObjectBroker getInstance() {
-		//TODO
-	}
 	
 	public CommunicationModule getCommunicationModule() {
 		//TODO
