f8125c756f4615e34548c5f0bb6e0d57158b7b42
KnowYourAPI <addictedtoicetea@gmail.com>
2013-05-26 19:42:24 +0200

    Private read/write helper-Methoden geschrieben

--- src/mware_lib/CommunicationModule.java
+++ src/mware_lib/CommunicationModule.java
@@ -1,8 +1,40 @@
 package mware_lib;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class CommunicationModule {
 	
 	public void send(String message, String host, int port) {
 		//TODO
+	}
+	
+	public String sendAndReceive(String message, String host, int port) {
+		String answer = null;
+		
+		try {
+			Socket socket = new Socket(host, port);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return answer;
+	}
+	
+	private void writeToServer(Socket socket, String message) throws IOException {
+		DataOutputStream outToServer = new DataOutputStream(socket.getOutputStream());
+		outToServer.writeBytes(message + '\n' );
+	}
+	
+	private String readFromServer(Socket socket) throws IOException {
+		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		return inFromServer.readLine();
 	}
 }
