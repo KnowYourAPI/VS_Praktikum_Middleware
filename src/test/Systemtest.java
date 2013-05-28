d564effb040e50e02ceb8ec9f7c26021170ebaff
KnowYourAPI <addictedtoicetea@gmail.com>
2013-05-28 16:34:58 +0200

    Testpackage eingefuehrt

---
+++ src/test/Systemtest.java
@@ -0,0 +1,12 @@
+package test;
+
+import mware_lib.ObjectBroker;
+
+public class Systemtest {
+
+	public static void main(String[] args) {
+		ObjectBroker objectBroker = ObjectBroker.init("localhost", 9000);
+
+	}
+
+}
