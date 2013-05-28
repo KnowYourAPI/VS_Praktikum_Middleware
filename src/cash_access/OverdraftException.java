ed6bbd7e8273c1ad2d27711257fe260ca8fb1ce3
Daniela Barths <daniela.barths@googlemail.com>
2013-05-28 18:05:45 +0200

    nicht zu implementierenden konstruktor entfernt

--- src/cash_access/OverdraftException.java
+++ src/cash_access/OverdraftException.java
@@ -4,10 +4,6 @@ public class OverdraftException extends Exception{
 
 	private static final long serialVersionUID = 1L;
 	
-	public OverdraftException() {
-		super();
-	}
-	
 	public OverdraftException(String message) {
 		super(message);
 	}
