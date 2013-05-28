a82fa0535a536e1741b2a80ed9190cb0f9a31144
Daniela Barths <daniela.barths@googlemail.com>
2013-05-28 18:22:00 +0200

    exceptions auch ohne massage werfbar

--- src/cash_access/OverdraftException.java
+++ src/cash_access/OverdraftException.java
@@ -4,6 +4,10 @@ public class OverdraftException extends Exception{
 
 	private static final long serialVersionUID = 1L;
 	
+	public OverdraftException() {
+		super();
+	}
+	
 	public OverdraftException(String message) {
 		super(message);
 	}
