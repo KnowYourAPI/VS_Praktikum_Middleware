ee35e1474be145a5fa4c4e20137168359cec9920
Daniela Barths <daniela.barths@googlemail.com>
2013-05-28 14:44:39 +0200

    NarrowCast implementiert

--- src/cash_access/TransactionImplBase.java
+++ src/cash_access/TransactionImplBase.java
@@ -1,5 +1,7 @@
 package cash_access;
 
+import bank_access.Account;
+
 public abstract class TransactionImplBase {
 	
 	public abstract void deposit(String accountID, double amount);
@@ -9,6 +11,10 @@ public abstract class TransactionImplBase {
 	public abstract double getBalance(String accountID);
 	
 	public static TransactionImplBase narrowCast(Object rawObjectRef) {
-		//TODO
+		String[] objectRefAry = ((String)rawObjectRef).split(":");
+		String host = objectRefAry[0];
+		int port = Integer.valueOf(objectRefAry[1]);
+		String name = objectRefAry[2];
+		return new Transaction(host, port, name);
 	}
 }
