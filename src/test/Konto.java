4a25c563c06da4ab39290660349f1783b18565cf
KnowYourAPI <addictedtoicetea@gmail.com>
2013-05-28 17:35:10 +0200

    Tests in progress

---
+++ src/test/Konto.java
@@ -0,0 +1,28 @@
+package test;
+
+import cash_access.OverdraftException;
+import cash_access.TransactionImplBase;
+
+public class Konto extends TransactionImplBase {
+
+	@Override
+	public void deposit(String accountID, double amount) {
+		System.out.println("Deposist called with: " + accountID + ", " + amount);
+	}
+
+	@Override
+	public void withdraw(String accountID, double amount)
+			throws OverdraftException {
+		System.out.println("Withdraw called with: " + accountID + ", " + amount);
+		
+	}
+
+	@Override
+	public double getBalance(String accountID) {
+		System.out.println("GetBalance called with: " + accountID);
+		return 42;
+	}
+
+
+
+}
--- src/test/Systemtest.java
+++ src/test/Systemtest.java
@@ -1,11 +1,25 @@
 package test;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import bank_access.AccountImplBase;
+
+import mware_lib.NameService;
 import mware_lib.ObjectBroker;
 
 public class Systemtest {
 
-	public static void main(String[] args) {
-		ObjectBroker objectBroker = ObjectBroker.init("localhost", 9000);
+	public static void main(String[] args) throws UnknownHostException {
+		String host = InetAddress.getLocalHost().getHostName();
+		ObjectBroker objectBroker = ObjectBroker.init(host, 9000);
+		NameService nameService = objectBroker.getNameService();
+		Konto konto = new Konto();
+		nameService.rebind(konto, "MeinKonto");
+		Object objectReference = nameService.resolve("MeinKonto");
+		AccountImplBase remoteKonto = AccountImplBase.narrowCast(objectReference);
+		remoteKonto.getBalance();
+		
 
 	}
 
