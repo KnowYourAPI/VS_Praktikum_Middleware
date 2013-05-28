5916a9c4b5a5e6f3655b2172cb9e010fbf1972c7
KnowYourAPI <addictedtoicetea@gmail.com>
2013-05-28 16:15:26 +0200

    Skeleton komplett

--- src/mware_lib/Skeleton.java
+++ src/mware_lib/Skeleton.java
@@ -2,7 +2,6 @@ package mware_lib;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
