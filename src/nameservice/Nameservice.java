720b262f14e70784bc57b7be80dfee03652ef985
Daniela Barths <daniela.barths@googlemail.com>
2013-05-29 09:51:50 +0200

    nameservice_access -> nameservice name geaendert

---
+++ src/nameservice/Nameservice.java
@@ -0,0 +1,23 @@
+package nameservice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Nameservice {
+
+private Map<String, String> nameToReference;
+
+public Nameservice() {
+this.nameToReference = new HashMap<String, String>();
+}
+
+public synchronized void rebind(String name, String reference) {
+System.out.println("Rebind: " + name + " -> " + reference);
+nameToReference.put(name, reference);
+}
+
+public synchronized String resolve(String name) {
+return nameToReference.get(name);
+}
+
+}
