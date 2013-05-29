411cb6bbfdd60930b9fb7b6918c8743dac585346
Daniela Barths <daniela.barths@googlemail.com>
2013-05-29 09:53:49 +0200

    Format geaendert

--- src/nameservice/Main.java
+++ src/nameservice/Main.java
@@ -1,13 +1,20 @@
 package nameservice;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import mware_lib.ObjectBroker;
+
 public class Main {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws UnknownHostException {
+		String host = InetAddress.getLocalHost().getHostName();
+		String nameserviceName = "nameservice";
+		int port = 9000;
 
+		Nameservice nameservice = new Nameservice();
+		ObjectBroker objectBroker = ObjectBroker.init(host, port, port);
+		objectBroker.getNameService().rebind(nameservice, nameserviceName);
 	}
 
 }
--- src/nameservice/Nameservice.java
+++ src/nameservice/Nameservice.java
@@ -5,19 +5,19 @@ import java.util.Map;
 
 public class Nameservice {
 
-private Map<String, String> nameToReference;
+	private Map<String, String> nameToReference;
 
-public Nameservice() {
-this.nameToReference = new HashMap<String, String>();
-}
+	public Nameservice() {
+		this.nameToReference = new HashMap<String, String>();
+	}
 
-public synchronized void rebind(String name, String reference) {
-System.out.println("Rebind: " + name + " -> " + reference);
-nameToReference.put(name, reference);
-}
+	public synchronized void rebind(String name, String reference) {
+		System.out.println("Rebind: " + name + " -> " + reference);
+		nameToReference.put(name, reference);
+	}
 
-public synchronized String resolve(String name) {
-return nameToReference.get(name);
-}
+	public synchronized String resolve(String name) {
+		return nameToReference.get(name);
+	}
 
 }
